0fc9b5efcd0e85376980276ec338c3659ce04b36
illichso <illichso@gmail.com>
2017-03-27 13:22:51 +0200

    add tests and application class

--- src/main/java/illich/so/BinaryTree.java
+++ src/main/java/illich/so/BinaryTree.java
@@ -44,17 +44,4 @@ public class BinaryTree {
 
         }
     }
-
-    public static void main(String[] args) {
-        BinaryTree theTree = new BinaryTree();
-        theTree.addNode(50, "Boss");
-        theTree.addNode(25, "Vise Pres");
-        theTree.addNode(15, "Office Manager");
-        theTree.addNode(30, "Secretary");
-        theTree.addNode(75, "Sales Manager");
-        theTree.addNode(85, "Salesman 1");
-
-        theTree.inOrderTraverseTree(theTree.getRoot());
-
-    }
 }
---
+++ src/main/test/java/illichso.so/BinaryTreeTest.java
@@ -0,0 +1,4 @@
+package illichso.so;
+
+public class BinaryTreeTest {
+}
